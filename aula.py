51b9103db42bac821381799fdceb3f4e21c8ff4c
Jasm <joaovictorasm203@gmail.com>
2025-08-21 22:26:22 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -1,15 +1,71 @@
-
-altura = float(input("Digite a sua altura em cms: "))
-genero = str(input("digite o seu genero: "))
-genero_possivel = "M","F"
-if altura in genero_possivel:
-    print("altura valida")
-else :
-    print("altura invalida")
 alturas = []
+generos = []
+
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+altura = float(input("Digite a altura em metros (ex: 1.75): "))
+
 alturas.append(altura)
-generos =[]
+
+
+genero = input("Digite o gênero M ou F : ").upper#colooca em maiusculo btw
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+genero = input("Digite o gênero M ou F : ").upper
+
 generos.append(genero)
 
 
-Media__a_total = alturas / 15 
+
+soma_alturas_masculino = 0
+num_masculino = 0
+num_feminino = 0
+ 
+for i in range(len(alturas)):
+  
+  if generos[i] == 'M':  #varivael i chamando como pessoas p diferenciar masuclino ou feminions
+    soma_alturas_masculino += alturas[i]
+    num_masculino += 1
+
+  elif generos[i] == 'F':
+    num_feminino += 1
+    
+maior_altura = max(alturas)
+menor_altura = min(alturas)
+print(f"A maior altura do grupo{maior_altura:}")
+print(f"A menor altura do grupo:{menor_altura:}")
+
+if num_masculino > 0:
+    media_masculino = soma_alturas_masculino / num_masculino
+    print(f"A média de altura do gênero Masculino é: {media_masculino:}")
+else :
+    print("nao existe pessoas do genero M")
+
+print(f"o numero de pessoas do genero feminino é: {num_feminino}")
+
+#PROF AINDA NAO ACABEI TENTANDO RESOLVER UM BUG
+!!
